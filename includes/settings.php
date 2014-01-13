5a3862ccea7dbdf8d78a679dfb1e9b724ebe7c50
Daniel Duan <danielduan88@yahoo.com>
2014-01-12 19:58:38 -0800

    settings in password

--- includes/settings.php
+++ includes/settings.php
@@ -270,3 +270,5 @@ $CONFIG['version'] = '1.4.6';
 //---PRESERVE ME---
 // Anything below this line will be preserved when the admin control panel rewrites
 // the settings. Useful for storing settings that don't/can't be changed from the control panel
+
+$adminDetails['root'] = '5e9662c96ffb02d0a86586f6e189e53d';
